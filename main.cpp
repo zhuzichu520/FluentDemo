77eb9c6b18e99ffdede20176c300e9f076d4298f
zhuzihcu <zhuzichu520@gmail.com>
2023-06-16 12:47:46 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,18 +1,37 @@
-﻿#include <QGuiApplication>
+#include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QQuickStyle>
 
-int main(int argc, char *argv[])
+#include <FramelessHelper/Quick/framelessquickmodule.h>
+#include <FramelessHelper/Core/private/framelessconfig_p.h>
+FRAMELESSHELPER_USE_NAMESPACE
+
+    int main(int argc, char *argv[])
 {
+    //将样式设置为Basic，不然会导致组件显示异常
+    qputenv("QT_QUICK_CONTROLS_STYLE","Basic");
+    FramelessHelper::Quick::initialize();
+    QGuiApplication::setOrganizationName("ZhuZiChu");
+    QGuiApplication::setOrganizationDomain("https://zhuzichu520.github.io");
+    QGuiApplication::setApplicationName("FluentUI");
     QGuiApplication app(argc, argv);
-    QQuickStyle::setStyle("Basic");
+#ifdef Q_OS_WIN // 此设置仅在Windows下生效
+    FramelessConfig::instance()->set(Global::Option::ForceHideWindowFrameBorder);
+#endif
+    FramelessConfig::instance()->set(Global::Option::DisableLazyInitializationForMicaMaterial);
+    FramelessConfig::instance()->set(Global::Option::CenterWindowBeforeShow);
+    FramelessConfig::instance()->set(Global::Option::ForceNonNativeBackgroundBlur);
+    FramelessConfig::instance()->set(Global::Option::EnableBlurBehindWindow);
+#ifdef Q_OS_MACOS
+    FramelessConfig::instance()->set(Global::Option::ForceNonNativeBackgroundBlur,false);
+#endif
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/FluentDemo/App.qml"_qs);
+    FramelessHelper::Quick::registerTypes(&engine);
+    const QUrl url(u"qrc:/FluentDemo/main.qml"_qs);
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
+        &app, [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        }, Qt::QueuedConnection);
     engine.load(url);
 
     return app.exec();
--- main.qml
+++ main.qml
@@ -1,23 +1,58 @@
-﻿import QtQuick
-import QtQuick.Window
-import QtQuick.Controls
+import QtQuick
 import QtQuick.Layouts
 import FluentUI
+import org.wangwenx190.FramelessHelper
 
 FluWindow {
-    id:app
+
+    id:window
+
     width: 640
     height: 480
-    visible: true
+    title:"FluentDemo"
 
+    FluAppBar {
+        id: title_bar
+        title: window.title
+        anchors {
+            top: parent.top
+            left: parent.left
+            right: parent.right
+        }
+        showDark: true
+    }
 
     FluButton{
-        text: "测试"
         anchors.centerIn: parent
+        text:"hello world"
         onClicked: {
-            showSuccess("点击了测试")
+            showSuccess("click hello world!")
         }
     }
 
+    FramelessHelper{
+        id:framless_helper
+        onReady: {
+            setTitleBarItem(title_bar)
+            moveWindowToDesktopCenter()
+            setHitTestVisible(title_bar.minimizeButton())
+            setHitTestVisible(title_bar.maximizeButton())
+            setHitTestVisible(title_bar.closeButton())
+            setHitTestVisible(title_bar.darkButton())
+            if (blurBehindWindowEnabled)
+                window.backgroundVisible = false
+            window.show()
+        }
+    }
+
+    Connections{
+        target: FluTheme
+        function onDarkChanged(){
+            if (FluTheme.dark)
+                FramelessUtils.systemTheme = FramelessHelperConstants.Dark
+            else
+                FramelessUtils.systemTheme = FramelessHelperConstants.Light
+        }
+    }
 
 }
